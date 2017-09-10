ed21c6eeabf116f1cb1523a957b37024d1cbe4af
Gaspar Nagy <gaspar.nagy@gmail.com>
2017-09-10 20:40:40 +0100

    add thank you page

--- GeekPizza1/GeekPizza1/ViewModels/EnterAddressViewModel.cs
+++ GeekPizza1/GeekPizza1/ViewModels/EnterAddressViewModel.cs
@@ -3,22 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using GeekPizza1.Models;
 using GeekPizza1.Services;
+using GeekPizza1.Views;
+using Xamarin.Forms;
 
 namespace GeekPizza1.ViewModels
 {
     public class EnterAddressViewModel : BaseViewModel
     {
-        private readonly Store _store;
-
         public Address DeliveryAddress { get; }
+        public ICommand PayCommand { get; }
 
         public EnterAddressViewModel(Store store)
         {
-            _store = store;
             Title = "Delivery Address";
             DeliveryAddress = store.Order.DeliveryAddress;
+            PayCommand = new Command(async () =>
+            {
+                await Navigation.PushAsync(new ThankYouPage(new ThankYouViewModel(store)));
+            });
         }
     }
 }
