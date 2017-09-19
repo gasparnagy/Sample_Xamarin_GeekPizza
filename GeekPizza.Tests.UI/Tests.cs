9a33d2e831a4981c911888dc75a8656f893eeedd
Gaspar Nagy <gaspar.nagy@gmail.com>
2017-09-19 21:43:49 +0200

    Add test for quantiry (not working)

--- GeekPizza.Tests.UI/Tests.cs
+++ GeekPizza.Tests.UI/Tests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using System.Threading;
 using NUnit.Framework;
 using Xamarin.UITest;
 using Xamarin.UITest.Queries;
@@ -29,6 +30,22 @@ namespace GeekPizza.Tests.UI
         public void AppLaunches()
         {
             app.Screenshot("First screen.");
+        }
+
+        [Test]
+        public void Test1()
+        {
+            app.Tap(e => e.Marked("NameLabel").All().Text("Chris Matts' GTW").Parent());
+            app.Back();
+            //app.Tap(e => e.Marked("NameLabel").All().Text("Uncle Bob's FitNesse").Parent());
+            //app.Back();
+            app.Tap(e => e.Marked("NameLabel").All().Text("Aslak Hellesøy's Cucumber").Parent());
+            app.Back();
+            app.Tap(e => e.Marked("NameLabel").All().Text("Aslak Hellesøy's Cucumber").Parent());
+            var text = app.Query(e =>
+                    e.Marked("NameLabel").All().Text("Aslak Hellesøy's Cucumber").Parent().All().Marked("QuantityLabel"))
+                .First().Text;
+            Assert.AreEqual("2", text);
         }
     }
 }
