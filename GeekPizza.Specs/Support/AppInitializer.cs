bf764ee695b5feb381741f9fdf881370c4c4d114
Gaspar Nagy <gaspar.nagy@gmail.com>
2017-09-20 22:49:30 +0800

    add specs project

---
+++ GeekPizza.Specs/Support/AppInitializer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+using Xamarin.UITest;
+
+namespace GeekPizza.Specs.Support
+{
+    public class AppInitializer
+    {
+        public static IApp StartApp(Platform platform)
+        {
+            if (platform == Platform.Android)
+            {
+                return ConfigureApp
+                    .Android
+                    .StartApp();
+            }
+
+            return ConfigureApp
+                .iOS
+                .StartApp();
+        }
+    }
+}
+
